eac5248e5995d878af89ae221235c0c8934fadbc
Albin "albinek" Sadowski <albinek@albinek.com>
2018-01-10 11:52:16 +0100

    Improve unit tests for Linux

--- Tests/SwiftyBlockchainTests/BlockTests.swift
+++ Tests/SwiftyBlockchainTests/BlockTests.swift
@@ -5,18 +5,45 @@ final class BlockTests: XCTestCase {
     
     func testBlockHash() {
         let block1 = Block(previousHash: 0, transactions: ["test transaction 1"])
-        XCTAssertEqual(-8301865496324989072, block1.blockHash)
+        
+        #if os(Linux)
+            let expectedHashForBlock1 = -2639509807668851953
+        #else
+            let expectedHashForBlock1 = -8301865496324989072
+        #endif
+            
+        XCTAssertEqual(expectedHashForBlock1, block1.blockHash)
         
         let block2 = Block(previousHash: 0, transactions: ["test transaction 2"])
-        XCTAssertEqual(4704044732640366145, block2.blockHash)
+        
+        #if os(Linux)
+            let expectedHashForBlock2 = -3213349125456088923
+        #else
+            let expectedHashForBlock2 = 4704044732640366145
+        #endif
+        
+        XCTAssertEqual(expectedHashForBlock2, block2.blockHash)
     }
     
     func testDescription() {
         let block1 = Block(previousHash: 0, transactions: ["test transaction 1"])
-        XCTAssertEqual("blockHash: -8301865496324989072, transactions: [\"test transaction 1\"], previousHash: 0", block1.description)
+        
+        #if os(Linux)
+            let expectedHashForBlock1 = -2639509807668851953
+        #else
+            let expectedHashForBlock1 = -8301865496324989072
+        #endif
+        
+        XCTAssertEqual("blockHash: \(expectedHashForBlock1), transactions: [\"test transaction 1\"], previousHash: 0", block1.description)
+        
+        #if os(Linux)
+            let expectedHashForBlock2 = -3213349125456088923
+        #else
+            let expectedHashForBlock2 = 4704044732640366145
+        #endif
         
         let block2 = Block(previousHash: 0, transactions: ["test transaction 2"])
-        XCTAssertEqual("blockHash: 4704044732640366145, transactions: [\"test transaction 2\"], previousHash: 0", block2.description)
+        XCTAssertEqual("blockHash: \(expectedHashForBlock2), transactions: [\"test transaction 2\"], previousHash: 0", block2.description)
     }
     
     static var allTests = [
--- Tests/SwiftyBlockchainTests/BlockchainTests.swift
+++ Tests/SwiftyBlockchainTests/BlockchainTests.swift
@@ -22,11 +22,20 @@ final class BlockchainTests: XCTestCase {
         
         let block1 = Block(previousHash: 0, transactions: ["test transaction 1"])
         blockchain.append(block: block1)
-        XCTAssertEqual("Blockchain:\n  #0: blockHash: -8301865496324989072, transactions: [\"test transaction 1\"], previousHash: 0;\n", blockchain.description)
+        
+        #if os(Linux)
+            let expectedHashForBlock1 = -2639509807668851953
+            let expectedHashForBlock2 = -6097721999344705488
+        #else
+            let expectedHashForBlock1 = -8301865496324989072
+            let expectedHashForBlock2 = -7150782369769308554
+        #endif
+        
+        XCTAssertEqual("Blockchain:\n  #0: blockHash: \(expectedHashForBlock1), transactions: [\"test transaction 1\"], previousHash: 0;\n", blockchain.description)
         
         let block2 = Block(previousHash: 0, transactions: ["test transaction 1", "test transaction 2"])
         blockchain.append(block: block2)
-        XCTAssertEqual("Blockchain:\n  #0: blockHash: -8301865496324989072, transactions: [\"test transaction 1\"], previousHash: 0;\n  #1: blockHash: -7150782369769308554, transactions: [\"test transaction 1\", \"test transaction 2\"], previousHash: 0;\n", blockchain.description)
+        XCTAssertEqual("Blockchain:\n  #0: blockHash: \(expectedHashForBlock1), transactions: [\"test transaction 1\"], previousHash: 0;\n  #1: blockHash: \(expectedHashForBlock2), transactions: [\"test transaction 1\", \"test transaction 2\"], previousHash: 0;\n", blockchain.description)
     }
     
     static var allTests = [
