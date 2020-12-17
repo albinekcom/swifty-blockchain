21d056bf320c399e0475d0e21779bc17ab8f5029
Albin "albinek" Sadowski <albinek@albinek.com>
2020-12-17 22:02:16 +0100

    Update Travis CI file

--- Package.swift
+++ Package.swift
@@ -1,20 +1,20 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.3
 
 import PackageDescription
 
 let package = Package(
     name: "SwiftyBlockchain",
-    
+
     targets: [
         .target(
             name: "SwiftyBlockchain",
             dependencies: ["SwiftyBlockchainCore"]
         ),
-        
+
         .target(
             name: "SwiftyBlockchainCore"
         ),
-        
+
         .testTarget(
             name: "SwiftyBlockchainTests",
             dependencies: ["SwiftyBlockchainCore"]
